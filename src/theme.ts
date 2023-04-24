79fa886a7fad2552d7befa613bb90f8d7cec3b58
Blake Swineford <blake@blakeswineford.com>
2023-04-24 09:44:36 -0400

    Customize the theme

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'dark',
-  useSystemColorMode: false,
+  // useSystemColorMode: false,
 };
 
 const theme = extendTheme({
@@ -15,16 +15,25 @@ const theme = extendTheme({
     }),
   },
   colors: {
-    dark: {
-      bg: '#000000',
+    gray: {
+      50: '#f9f9f9',
+      100: '#ededed',
+      200: '#d3d3d3',
+      300: '#b3b3b3',
+      400: '#a0a0a0',
+      500: '#898989',
+      600: '#6c6c6c',
+      700: '#202020',
+      800: '#12121',
+      900: '#111',
     },
-    light: {
-      bg: '#F9F9F9',
+    // light: {
+      // bg: '#F9F9F9',
       // bg: '#FAFAFA', for off-white
       // bg: '#F8F7F3', Mercedes paper white
       // bg: '#F9F9F9', Apople's Ghost White
       // bg: '#FAFAFB', Original color
-    },
+    // },
   },
 });
 
