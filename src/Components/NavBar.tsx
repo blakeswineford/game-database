ed71cb8de477284cb781a84ef5910d8fc07777a0
Blake Swineford <blake@blakeswineford.com>
2023-04-24 08:43:03 -0400

    Implement search

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/lsso.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Prop {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
